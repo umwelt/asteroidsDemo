bf01429cb19ea66f13b2f9ccaea3d8c364421a01
supertramp <hugo@kode.zone>
2023-05-30 13:56:58 +0100

    clean: remove old comments

--- client/src/redux/reducers/index.ts
+++ client/src/redux/reducers/index.ts
@@ -2,7 +2,6 @@ import { combineReducers, Reducer } from "redux";
 import asteroidReducer, { AsteroidState } from './asteroidReducer';
 import Asteroid from "../../models/asteroid";
 
-// Define the root state shape
 export interface RootState {
 	asteroids: AsteroidState;
 	selectedAsteroid: Asteroid | null;
@@ -12,7 +11,6 @@ export interface RootState {
 const rootReducer: Reducer<RootState> = combineReducers<RootState>({
 	asteroids: asteroidReducer,
 	selectedAsteroid: (state = null, action) => {
-		// Add your logic for handling the selectedAsteroid state updates
 		return state;
 	},
 });
